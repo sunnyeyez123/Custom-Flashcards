87a710dc1483bbd462d39cf512b174f99380e9c3
Jasmine Lawrence <lawrence.jasmine@gmail.com>
2018-04-02 23:37:13 -0700

    update icon for set list empty state

--- app/src/main/java/com/jasminelawrence/customflashcards/MainActivity.java
+++ app/src/main/java/com/jasminelawrence/customflashcards/MainActivity.java
@@ -18,11 +18,11 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_add_set);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Add set", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
