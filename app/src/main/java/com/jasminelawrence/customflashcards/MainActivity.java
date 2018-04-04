07c0fcda4696b0f892670316ff38506bb7bd4a13
Jasmine Lawrence <lawrence.jasmine@gmail.com>
2018-04-03 21:08:32 -0700

    Create test button to launch card editing

--- app/src/main/java/com/jasminelawrence/customflashcards/MainActivity.java
+++ app/src/main/java/com/jasminelawrence/customflashcards/MainActivity.java
@@ -27,6 +27,11 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public void showTerm(View view){
+        Intent intent = new Intent(MainActivity.this, EditCardActivity.class);
+        startActivity(intent);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
