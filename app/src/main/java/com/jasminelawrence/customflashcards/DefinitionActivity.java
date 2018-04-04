3f23e1e34ab7bd7acb953719c69c685fcb9f4bf8
Jasmine Lawrence <lawrence.jasmine@gmail.com>
2018-04-03 23:26:28 -0700

    add up and flip menu options for definitions

--- app/src/main/java/com/jasminelawrence/customflashcards/DefinitionActivity.java
+++ app/src/main/java/com/jasminelawrence/customflashcards/DefinitionActivity.java
@@ -1,7 +1,11 @@
 package com.jasminelawrence.customflashcards;
 
+import android.content.Intent;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class DefinitionActivity extends AppCompatActivity {
 
@@ -9,5 +13,33 @@ public class DefinitionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_definition);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_term, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_flip_card:
+                Intent intent = new Intent(DefinitionActivity.this,TermActivity.class);
+                startActivity(intent);
+                return true;
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
